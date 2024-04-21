fdf1d8f764f1d38366174216c82a0a6ca67f4fc6
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-21 02:39:01 +0200

    Update browser_manager.py

--- browser_manager.py
+++ browser_manager.py
@@ -17,10 +17,10 @@ from robocorp.tasks import get_output_dir
 
 #oppening the site aljazeera.com
 @task
-def opening_the_news_Site(self):
-    self.url ="https://www.aljazeera.com/"
+def opening_the_news_Site():
+    url ="https://www.aljazeera.com/"
     # logger.info("Opening the news site.")
-    self.browser = Selenium(auto_close = False)
+    browser = Selenium(auto_close = False)
     
     # Define Chrome options to disable popup blocking
     options = [
@@ -31,7 +31,7 @@ def opening_the_news_Site(self):
     
 
     # Open browser with specified options
-    self.browser.open_available_browser(url, 
+    browser.open_available_browser(url, 
                                         browser_selection="Chrome", 
                                         options=options)
     # browser.close_all_browsers()
@@ -39,8 +39,8 @@ def opening_the_news_Site(self):
     print("oppend browser at last")
 
 @task
-def search_the_phrase(self):
-    self.phrase = "Business"
+def search_the_phrase():
+    phrase = "Business"
     print("inside search method")
     if(self.browser):
         print("found browser")
@@ -50,7 +50,7 @@ def search_the_phrase(self):
     # if the site contains collecting cookies 
     try:
         print("inside tyr")
-        self.browser.click_button('Allow all')
+        browser.click_button('Allow all')
         print("it clicked allow")
 
     except:
@@ -58,24 +58,24 @@ def search_the_phrase(self):
         pass
     # finding the serach icon and field
     locator1 = "//button[@aria-pressed='false']//*[name()='svg']"
-    self.browser.wait_until_page_contains_element(locator1, timeout=10)
-    self.browser.click_element(locator1)
+    browser.wait_until_page_contains_element(locator1, timeout=10)
+    browser.click_element(locator1)
 
     # inserting the search phrase in the input field
-    self.browser.input_text("//input[@placeholder='Search']",phrase)
-    self.browser.click_button("//button[@aria-label='Search Al Jazeera']")
+    browser.input_text("//input[@placeholder='Search']",phrase)
+    browser.click_button("//button[@aria-label='Search Al Jazeera']")
     # self.browser.close_all_browsers()
 
 
     # Trying to find it there is a realated articles with the search phrase
     try:
         locator2 = "//select[@id='search-sort-option']"
-        self.browser.wait_until_element_is_visible(locator2, timeout=10)
-        self.browser.click_element(locator2)
+        browser.wait_until_element_is_visible(locator2, timeout=10)
+        browser.click_element(locator2)
         print("finding the search area")
     except Exception as e:
         print(e, ": No news associated with the search phrase")
     # sort by time
     dropdown_locator = "//select[@id='search-sort-option']/option[1]" 
-    self.browser.click_element(dropdown_locator)
+    browser.click_element(dropdown_locator)
     print("completed opening and searching")
